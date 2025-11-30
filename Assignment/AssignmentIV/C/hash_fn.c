1096cc3c289ac9a55b4e27bd5ee13b1aeecfb085
Ting-ZhenChan <Sofe231436935@gmail.com>
2025-11-30 21:16:55 +0800

    final commit

--- Assignment/AssignmentIV/C/hash_fn.c
+++ Assignment/AssignmentIV/C/hash_fn.c
@@ -9,9 +9,17 @@
    Development History:
     - 2025/11/18: Initial implementation
     - 2025/11/24: Refactored to use hash_fn.h
+    - 2025/11/30: include References 
 
    Developer: Ting-ZhenChang <Sofe231436935@gmail.com>
  */
+
+    //References:
+        // Donald E. Knuth, The Art of Computer Programming, Volume 3: Sorting and Searching.Section 6.4 — “Hashing by Multiplication”.
+        // https://github.com/Code42Cate/The-Art-of-Computer-Programming/blob/master/Volume3.pdf
+        // Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein,Introduction to Algorithms (CLRS), Chapter 11 — “Hash Functions”, string hashing discussion.
+        // Polynomial rolling hash.” Wikipedia, The Free Encyclopedia.
+
 
 #include "hash_fn.h"
 
